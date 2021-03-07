5854711e4ca766671b4d719f8b2d75146ec31399
FihCapua <fihcapua@gmail.com>
2021-03-06 21:12:31 -0300

    fix: problems to merge

--- src/theme/typographVariants.js
+++ src/theme/typographVariants.js
@@ -31,20 +31,8 @@ export const typographyVariants = {
   },
   // [smallestException]
   smallestException: {
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-    fontSize: '12px',
-    fontWeight: '400',
-    lineHeight: 1, // 100%
-=======
     fontSize: '11px',
     fontWeight: '400',
     lineHeight: 1.5, // 150%
->>>>>>> Stashed changes
-=======
-    fontSize: '14px',
-    fontWeight: '400',
-    lineHeight: 1.5, // 105%
->>>>>>> 43f5921397e57c10e14f9393e9583affb95bdf30
   },
 };
