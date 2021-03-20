f3873af8b2e0aa7667a35eaa6f61a1b7abeb2f1c
FihCapua <fihcapua@gmail.com>
2021-03-19 23:18:15 -0300

    feat(structure): organize menu structure, include error page and reestructure about page

---
+++ pages/about/about.js
@@ -0,0 +1,23 @@
+/* eslint-disable max-len */
+import React from 'react';
+import { AboutWrapper } from './styles/AboutWrapper';
+
+export default function About() {
+  return (
+    // eslint-disable-next-line react/jsx-filename-extension
+    <AboutWrapper>
+      <AboutWrapper.Title>
+        Sobre Mim
+      </AboutWrapper.Title>
+      <AboutWrapper.Content>
+        <AboutWrapper.Image />
+        <AboutWrapper.Text>
+          Trabalhei 6 anos como web designer atuando em agências de marketing digital, e empresas.
+          Em busca de novos desafios e experiências optei pela transição da minha carreira como front-end em 2019,
+          dando continuidade ao meu projeto de aprendizado e de troca de conhecimentos na área de tecnologia,
+          desenvolvimento e comunicação.
+        </AboutWrapper.Text>
+      </AboutWrapper.Content>
+    </AboutWrapper>
+  );
+}
